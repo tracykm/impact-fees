23568db12a68f85e5951d5a6cbdbdd7e78c63815
Tracy <tmullen@clearblade.com>
2019-12-03 22:05:07 -0600

    basic table

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import dataJS from './data/cleaned/s2019.json';
-import { JurisdictionData } from './types';
-
-const data: JurisdictionData[] = dataJS
+import { BasicTable } from './components/Table'
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {JSON.stringify(data[0])}
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BasicTable/>
       </header>
     </div>
   );
