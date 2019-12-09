e5fb9497fe22af86e9661bb7189cee3bcf2197b1
Tracy <tmullen@clearblade.com>
2019-12-08 23:23:09 -0600

    one set of filters

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,42 +1,16 @@
-import React from 'react';
+import React, { useState } from "react";
 //@ts-ignore
-import {useTable, useSortBy, usePagination, useFilters} from 'react-table';
-import data from '../data/cleaned/nestedData.json';
-import {DollarCell, DateCell} from './Cell';
-import TableStyles from './TableStyles';
+import { useTable, useSortBy, usePagination, useFilters } from "react-table";
+import data from "../data/cleaned/nestedData.json";
+import { DollarCell, DateCell } from "./Cell";
+import TableStyles from "./TableStyles";
 const dataJS = Object.values(data);
 
-// Define a default UI for filtering
-function DefaultColumnFilter({
-  column: {filterValue, preFilteredRows, setFilter},
-}) {
-  const count = preFilteredRows.length;
-
-  return (
-    <input
-      value={filterValue || ''}
-      onChange={e => {
-        setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
-      }}
-      placeholder={`Search ${count} records...`}
-    />
-  );
-}
-
-function Table({columns, data}) {
-  const defaultColumn = React.useMemo(
-    () => ({
-      // Let's set up our default Filter UI
-      Filter: DefaultColumnFilter,
-    }),
-    []
-  );
-
+function Table({ columns, data }) {
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
     prepareRow,
     page, // Instead of using 'rows', we'll use page,
     // which has only the rows for the active page
@@ -48,21 +22,43 @@ function Table({columns, data}) {
     nextPage,
     previousPage,
     setPageSize,
-    state: {pageIndex, pageSize},
+    state: { pageIndex, pageSize },
+    ...args
   } = useTable(
     {
       columns,
       data,
-      initialState: {pageSize: 20},
-      defaultColumn,
+      initialState: { pageSize: 20 }
     },
     useFilters,
     useSortBy,
     usePagination
   );
 
+  const [columnFilter, setColumnFilter] = useState(2);
+  const currentCol = args.columns[0].columns[columnFilter];
+  debugger;
   return (
     <>
+      <select
+        onChange={e => {
+          args.columns[0].columns[Number(e.target.value)].setFilter(
+            currentCol.filterValue
+          );
+          currentCol.setFilter(undefined);
+          setColumnFilter(Number(e.target.value));
+        }}
+      >
+        <option value="2">Jurisdiction</option>
+        <option value="1">County</option>
+        <option value="0">State</option>
+      </select>
+      <input
+        onChange={e => {
+          currentCol.setFilter(e.target.value);
+        }}
+        placeholder="search..."
+      />
       <table {...getTableProps()}>
         <thead>
           {headerGroups.map(headerGroup => (
@@ -71,18 +67,14 @@ function Table({columns, data}) {
                 // Add the sorting props to control sorting. For this example
                 // we can add them into the header props
                 <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {column.render('Header')}
+                  {column.render("Header")}
                   {/* Add a sort direction indicator */}
                   <span>
                     {column.isSorted
                       ? column.isSortedDesc
-                        ? ' 🔽'
-                        : ' 🔼'
-                      : ''}
-
-                    <div>
-                      {column.canFilter ? column.render('Filter') : null}
-                    </div>
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
                   </span>
                 </th>
               ))}
@@ -96,7 +88,7 @@ function Table({columns, data}) {
               <tr {...row.getRowProps()}>
                 {row.cells.map(cell => {
                   return (
-                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
                   );
                 })}
               </tr>
@@ -111,25 +103,25 @@ function Table({columns, data}) {
       */}
       <div className="pagination">
         <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-          {'<<'}
-        </button>{' '}
+          {"<<"}
+        </button>{" "}
         <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-          {'<'}
-        </button>{' '}
+          {"<"}
+        </button>{" "}
         <button onClick={() => nextPage()} disabled={!canNextPage}>
-          {'>'}
-        </button>{' '}
+          {">"}
+        </button>{" "}
         <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-          {'>>'}
-        </button>{' '}
+          {">>"}
+        </button>{" "}
         <span>
-          Page{' '}
+          Page{" "}
           <strong>
             {pageIndex + 1} of {pageOptions.length}
-          </strong>{' '}
+          </strong>{" "}
         </span>
         <span>
-          | Go to page:{' '}
+          | Go to page:{" "}
           <input
             type="number"
             defaultValue={pageIndex + 1}
@@ -137,9 +129,9 @@ function Table({columns, data}) {
               const page = e.target.value ? Number(e.target.value) - 1 : 0;
               gotoPage(page);
             }}
-            style={{width: '100px'}}
+            style={{ width: "100px" }}
           />
-        </span>{' '}
+        </span>{" "}
         <select
           value={pageSize}
           onChange={e => {
@@ -157,131 +149,90 @@ function Table({columns, data}) {
   );
 }
 
-// This is a custom filter UI that uses a
-// slider to set the filter value between a column's
-// min and max values
-function SliderColumnFilter({
-  column: {filterValue, setFilter, preFilteredRows, id},
-}) {
-  // Calculate the min and max
-  // using the preFilteredRows
-
-  const [min, max] = React.useMemo(() => {
-    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
-    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
-    preFilteredRows.forEach(row => {
-      min = Math.min(row.values[id], min);
-      max = Math.max(row.values[id], max);
-    });
-    return [min, max];
-  }, [id, preFilteredRows]);
-
-  return (
-    <>
-      <input
-        type="range"
-        min={min}
-        max={max}
-        value={filterValue || min}
-        onChange={e => {
-          setFilter(parseInt(e.target.value, 10));
-        }}
-      />
-      <button onClick={() => setFilter(undefined)}>Off</button>
-    </>
-  );
-}
-
 const DetailColumns = name => [
   {
-    Header: 'Total',
+    Header: "Total",
     accessor: `DataEntries[1].${name}.Total`,
-    Cell: DollarCell,
-    Filter: SliderColumnFilter,
+    Cell: DollarCell
   },
   {
-    Header: 'NonUtil',
+    Header: "NonUtil",
     accessor: `DataEntries[1].${name}.NonUtil`,
-    Cell: DollarCell,
+    Cell: DollarCell
   },
   {
-    Header: 'Sewer',
+    Header: "Sewer",
     accessor: `DataEntries[1].${name}.Sewer`,
-    Cell: DollarCell,
+    Cell: DollarCell
   },
   {
-    Header: 'Fire',
+    Header: "Fire",
     accessor: `DataEntries[1].${name}.Fire`,
-    Cell: DollarCell,
+    Cell: DollarCell
   },
   {
-    Header: 'Parks',
+    Header: "Parks",
     accessor: `DataEntries[1].${name}.Parks`,
-    Cell: DollarCell,
+    Cell: DollarCell
   },
   {
-    Header: 'Fire',
-    accessor: `DataEntries[1].${name}.Fire`,
-    Cell: DollarCell,
-  },
-  {
-    Header: 'Library',
+    Header: "Library",
     accessor: `DataEntries[1].${name}.Library`,
-    Cell: DollarCell,
+    Cell: DollarCell
   },
   {
-    Header: 'Police',
+    Header: "Police",
     accessor: `DataEntries[1].${name}.Police`,
-    Cell: DollarCell,
-  },
+    Cell: DollarCell
+  }
 ];
 
 export function BasicTable() {
   const columns = React.useMemo(
     () => [
       {
-        Header: 'Basic Info',
+        Header: "Basic Info",
         columns: [
           {
-            Header: 'State',
-            accessor: 'State',
-            width: 500,
+            Header: "State",
+            accessor: "State",
+            width: 500
           },
           {
-            Header: 'County',
-            accessor: 'County',
+            Header: "County",
+            accessor: "County"
           },
           {
-            Header: 'Jurisdiction',
-            accessor: 'Jurisdiction',
+            Header: "Jurisdiction",
+            accessor: "Jurisdiction"
           },
           {
-            Header: 'Updated',
-            accessor: 'DataEntries[1].Updated',
-            Cell: DateCell,
-          },
-        ],
+            Header: "Updated",
+            accessor: "DataEntries[1].Updated",
+            Cell: DateCell
+          }
+        ]
       },
       {
-        Header: 'Single Family',
-        columns: DetailColumns('SingleFamily'),
+        Header: "Single Family",
+        columns: DetailColumns("SingleFamily")
       },
       {
-        Header: 'Multi Family',
-        columns: DetailColumns('MultiFamily'),
+        Header: "Multi Family",
+        columns: DetailColumns("MultiFamily")
       },
       {
-        Header: 'Retail',
-        columns: DetailColumns('Retail'),
+        Header: "Retail",
+        columns: DetailColumns("Retail")
       },
       {
-        Header: 'Office',
-        columns: DetailColumns('Office'),
+        Header: "Office",
+        columns: DetailColumns("Office")
       },
       {
-        Header: 'Industrial',
-        columns: DetailColumns('Industrial'),
-      },
+        Header: "Industrial",
+        columns: DetailColumns("Industrial")
+      }
     ],
     []
   );
