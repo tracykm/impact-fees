f72832168e87f9c956f7dcd8cce53f93faadc912
Tracy <tmullen@clearblade.com>
2019-12-22 18:47:12 -0600

    data

--- src/data/convert.js
+++ src/data/convert.js
@@ -1,18 +1,23 @@
-const s2019 = require("./raw/s2019");
+// const s2019 = require("./raw/s2019");
 const s2015 = require("./raw/s2015");
 const s2012 = require("./raw/s2012");
 const s2011 = require("./raw/s2011");
 const s2010 = require("./raw/s2010");
+// const s2009 = require("./raw/s2009");
+// const s2008 = require("./raw/s2008");
 const s2007 = require("./raw/s2007");
-const sample = require("./raw/sample");
+// const sample = require("./raw/sample");
 
 const { cleanData } = require("./cleanData");
 const { nestData } = require("./nestData");
+const { getAverages } = require("./getAverages");
 var fs = require("fs");
 
 const allSets = {
   // sample
   s2007,
+  // s2008,
+  // s2009,
   s2010,
   s2011,
   s2012,
@@ -40,3 +45,10 @@ fs.writeFile(
   "utf8",
   () => {}
 );
+
+// fs.writeFile(
+//   `src/data/cleaned/stateAverages.json`,
+//   JSON.stringify(getAverages(d => d.State === "CO")),
+//   "utf8",
+//   () => {}
+// );
