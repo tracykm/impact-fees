60f50627fda37c3b0875ba94c3c8bc2a09d0d707
Tracy <tmullen@clearblade.com>
2019-12-24 13:50:40 -0600

    remove unused packages

--- src/components/ButtonOptions.tsx
+++ src/components/ButtonOptions.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { ButtonGroup, Button } from "reactstrap";
 import styled from "styled-components";
 
 interface ButtonOptionProps {
   selected: boolean;
 }
 
-const ButtonOption = styled(Button)`
+const ButtonOption = styled.button`
   z-index: ${(props: ButtonOptionProps) => (props.selected ? 1 : 0)};
 `;
 
@@ -20,19 +19,22 @@ export const ButtonOptions = ({
   value: string | number;
 }) => {
   return (
-    <ButtonGroup>
+    <div className="btn-group" role="group">
       {options.map(d => (
         <ButtonOption
           selected={d.value === value}
           key={d.value}
           onClick={() => onChange(d.value)}
           className={
-            value === d.value ? "btn-outline-primary" : "btn-outline-secondary"
+            "btn " +
+            (value === d.value
+              ? "btn-outline-primary"
+              : "btn-outline-secondary")
           }
         >
           {d.name}
         </ButtonOption>
       ))}
-    </ButtonGroup>
+    </div>
   );
 };
