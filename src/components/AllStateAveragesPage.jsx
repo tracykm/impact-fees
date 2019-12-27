a394630f868be240ba7716086704334d20db1704
Tracy <tmullen@clearblade.com>
2019-12-27 17:55:51 -0600

    add date control to state averages

--- src/components/AllStateAveragesPage.jsx
+++ src/components/AllStateAveragesPage.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { STATES } from "../types";
 import stateAverages from "../data/cleaned/stateAverages.json";
 import { AllDetailColumns } from "./columns";
 import { Table } from "./Table";
+import { ButtonsOrDropdown } from "./ButtonsOrDropdown";
 
 const data = [];
 
@@ -15,11 +16,23 @@ Object.keys(stateAverages).forEach(State => {
   }
   return undefined;
 });
+const yearOpts = data[0].DataEntries.map((d, i) => {
+  return { value: i, name: new Date(d.Updated).getFullYear() };
+});
 
 export const AllStateAveragesPage = () => {
-  const path = `DataEntries[0].`;
+  const [stateSelected, changeState] = useState(0);
+  const path = `DataEntries[${stateSelected}].`;
   return (
     <>
+      <div>
+        <ButtonsOrDropdown
+          options={yearOpts}
+          onChange={val => {
+            changeState(Number(val));
+          }}
+        />
+      </div>
       <Table
         columns={[
           {
