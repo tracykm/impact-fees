95c40c18c6e531c74e1830fe017cb317b02d0ff9
Tracy <tmullen@clearblade.com>
2019-12-27 18:36:11 -0600

    format labels

--- src/components/AllStateAveragesPage.jsx
+++ src/components/AllStateAveragesPage.jsx
@@ -4,8 +4,10 @@ import stateAverages from "../data/cleaned/stateAverages.json";
 import { AllDetailColumns } from "./columns";
 import { Table } from "./Table";
 import { ButtonsOrDropdown } from "./ButtonsOrDropdown";
+import { BarChart, CartesianGrid, XAxis, YAxis, Bar, Tooltip } from "recharts";
+import { formatMoney } from "./Cell";
 
-const data = [];
+let data = [];
 
 Object.keys(stateAverages).forEach(State => {
   if (stateAverages[State][0]) {
@@ -16,23 +18,49 @@ Object.keys(stateAverages).forEach(State => {
   }
   return undefined;
 });
+
+data = data.sort((d1, d2) => {
+  return (
+    d2.DataEntries[0].SingleFamily.Total - d1.DataEntries[0].SingleFamily.Total
+  );
+});
+
 const yearOpts = data[0].DataEntries.map((d, i) => {
   return { value: i, name: new Date(d.Updated).getFullYear() };
 });
 
+function getShortStateName(val) {
+  return STATES.find(d => d.name === val).short_name;
+}
+
 export const AllStateAveragesPage = () => {
-  const [stateSelected, changeState] = useState(0);
-  const path = `DataEntries[${stateSelected}].`;
+  const [yearSelected, changeYear] = useState(0);
+  const path = `DataEntries[${yearSelected}].`;
+  const longerPath = `${path}SingleFamily.Total`;
   return (
     <>
       <div>
         <ButtonsOrDropdown
           options={yearOpts}
           onChange={val => {
-            changeState(Number(val));
+            changeYear(Number(val));
           }}
         />
       </div>
+
+      <BarChart width={1200} height={250} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="State" tickFormatter={getShortStateName} />
+        <YAxis tickFormatter={formatMoney} />
+        <Tooltip
+          formatter={(val, name) => [
+            formatMoney(val),
+            name.split(".")[1] + " " + name.split(".")[2]
+          ]}
+        />
+        <Bar dataKey={longerPath} fill="#56d19d" />
+      </BarChart>
+
       <Table
         columns={[
           {
@@ -43,15 +71,6 @@ export const AllStateAveragesPage = () => {
         ]}
         data={data}
       />
-      {STATES.map(s => {
-        const samples =
-          stateAverages[s.short_name][0] &&
-          stateAverages[s.short_name][0].SampleSize;
-        if (!samples) {
-          return <span className="p-2">{s.name} </span>;
-        }
-        return undefined;
-      })}
     </>
   );
 };
